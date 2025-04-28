9bc643fc71fadf2f675ff1343045648db44c1d10
patrickmulcahy2 <patrick.mulcahy@edengeopower.com>
2025-04-27 22:41:28 -0400

    Update manual_control.py

--- project/manual_control.py
+++ project/manual_control.py
@@ -7,9 +7,11 @@ from .config import app, socketio, reqPosition
 def init_manualControl_handlers():
 	@socketio.on('sendPolarCoordinates')
 	def recievePolarCoorinates_manual(data):
+		global reqPosition
 
 		rhoReq = data.get('rho')
 		thetaReq = data.get('theta')
 
 		reqPosition["rhoReq"] = rhoReq
 		reqPosition["thetaReq"] = thetaReq
+
